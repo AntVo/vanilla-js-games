ce5756d6a40d9783e064831614f74e5c250a204d
antoinevo <antoine.monarch@gmail.com>
2018-08-19 20:02:15 -0700

    Snake

--- snake/script.js
+++ snake/script.js
@@ -5,32 +5,67 @@ var context = canvas.getContext('2d');
 var HEIGHT = canvas.height;
 var WIDTH = canvas.width;
 var tileSize = 25;
-var timer = setInterval(updateGame, 500);
-
+var	snake = [[250, 250], [250, 225], [250, 200]];
+var direction = "up";
+var timer = setInterval(updateGame, 200);
 context.fillStyle = 'rgb(75, 75, 75)';
-context.fillRect(0, 0, WIDTH, HEIGHT);
+context.strokeStyle="green";
 
 
-
-function Snake(){
-	this.head = null;
-	this.direction = "RIGHT";
-	this.length = 4;
+document.onkeydown = function(e){
+	switch (e.keyCode){
+		case 37:
+			changeDirection('left');
+			break;
+		case 38:
+			changeDirection('up');
+			break;
+		case 39:
+			changeDirection('right');
+			break;
+		case 40:
+			changeDirection('down');
+			break;
+		}
 }
 
-function Food(){
-	this.x = Math.floor(Math.random() * 20 ) * 25;
-	this.y = Math.floor(Math.random() * 20 ) * 25;
-	this.randomizeCoordinates = function(){
-		this.x = Math.floor(Math.random() * 20 ) * 25;
-		this.y = Math.floor(Math.random() * 20 ) * 25;
+function moveSnake(){
+	 let firstNode = snake[0];
+   switch(direction){
+   	case "up":
+   		firstNode[1] -= tileSize;
+   		break;
+   	case "down":
+   		firstNode[1] += tileSize;
+   		break;
+   	case "left":
+   		firstNode[0] -= tileSize;
+   		break;
+   	case "right":
+   		firstNode[0] += tileSize;
+   		break;
+   }
+   snake.unshift(firstNode);
+   snake.pop();
+}
 
-	}
+function drawSnake(){
+	snake.forEach((node) => {
+		context.strokeRect(node[0], node[1], 25, 25);
+	})
+}
+
+function changeDirection(dir){
+	direction = dir;
 }
 
 function updateGame(){
-
-	console.log('updating');
+	context.clearRect(0, 0, WIDTH, HEIGHT);
+	context.fillRect(0, 0, WIDTH, HEIGHT);
+	context.beginPath();
+	moveSnake();
+	drawSnake();
+	context.closePath();
 	// Check for Collission
 	// Move the Snake
 	// Draw Snake on Canvas
